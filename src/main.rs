7ce0008c03a831c70dd513bbe4c56bc571d905fc
宮下 陸 <x23g082@chiba-fjb.ac.jp>
2023-06-01 18:50:18 +0900

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,29 @@
-use std::{process::Command, env};
+use ansi_term::Color as OtherColor;
 use crossterm::{
-    style::{Color, SetForegroundColor, ResetColor},
+    style::{Color, ResetColor, SetForegroundColor},
     ExecutableCommand,
 };
-use ansi_term::Color as OtherColor;
-use image::{GenericImageView, ImageBuffer, Rgba, open};
-use image::Pixel;
 use image::io::Reader as ImageReader;
+use image::Pixel;
+use image::{open, GenericImageView, ImageBuffer, Rgba};
+use std::path::Path;
+use std::{env, process::Command};
 
 fn main() {
-    println!("{}■{}■{}■{}",
-    SetForegroundColor(Color::Cyan),
-    SetForegroundColor(Color::Green),
-    SetForegroundColor(Color::Yellow),
-    ResetColor);
-    // 入力ファイル名と出力ファイル名を指定
-    // let input_file = "input.mp4";
-    // let output_pattern = "output_frames\\image_%03d.png";
+    const CLEAR_SCREEN: &str = "\x1B[2J\x1B[1;1H";
+    let input_file = "input.mp4";
+    let output_dir = Path::new("output_frames");
+    let output_pattern = "output_frames\\image_%d.png";
 
-    // let ffmpegPath = "ffmpeg\\ffmpeg\\bin\\ffmpeg.exe";
-    
-    // let path = env::current_dir();
+    let ffmpegPath = "ffmpeg\\ffmpeg\\bin\\ffmpeg.exe";
+
     // println!("{:?}", path);
 
     // let MovieToBMP = Command::new(ffmpegPath)
     //     .arg("-i")
     //     .arg(input_file)
     //     .arg("-vcodec")
-    //     .arg("bmp")
+    //     .arg("png")
     //     .arg(output_pattern)
     //     .output()
     //     .expect("Failed is Codec");
@@ -40,37 +36,39 @@ fn main() {
     //     println!("Error");
     // }
 
-    println!("{:?}", env::current_dir());
-
-    let image_path = "output_frames\\input.png";
-    let result = open(image_path);
-
-    match result
-    {
-        Ok(image) =>
-        {
-            let (width, height) = image.dimensions();
-
-            // 画像の各ピクセルに対して色情報を取得し、出力する
-            for y in 0..height {
-                for x in 0..width {
-                    // ピクセルの色情報を取得する
-                    let pixel_color: Rgba<u8> = image.get_pixel(x, y).into();
+    for entry in output_dir.read_dir().expect("read_dir call failed") {
+        if let Ok(entry) = entry {
         
-                    // R, G, B の値を取得する
-                    let (red, green, blue, _) = pixel_color.channels4();
-                    // ANSIカラーを使ってテキストを出力する
-                    print!("{}■", SetForegroundColor(Color::Rgb{r:red, g:green, b:blue}));
+
+            let result = open(entry.path());
+
+            match result
+            {
+                Ok(image) =>
+                {
+                    let (width, height) = image.dimensions();
+
+                    // 画像の各ピクセルに対して色情報を取得し、出力する
+                    for y in 0..height {
+                        for x in 0..width {
+                            // ピクセルの色情報を取得する
+                            let pixel_color: Rgba<u8> = image.get_pixel(x, y).into();
+
+                            // R, G, B の値を取得する
+                            let (red, green, blue, _) = pixel_color.channels4();
+                            print!("{}■", SetForegroundColor(Color::Rgb{r:red, g:green, b:blue}));
+                        }
+                        println!();
+                    }
+                    println!("{}", CLEAR_SCREEN);
                 }
-                println!();
-            }
-        
-        }
 
-        Err(err) =>
-        {
-            println!("{}", err);
+                Err(err) =>
+                {
+                    println!("{}", err);
+                }
+            }
+            
         }
     }
-
 }
