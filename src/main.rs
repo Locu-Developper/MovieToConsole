d6a292c903d1af4b8a07559ee8bf6d61bba49b62
Locu-Developper <creator.0831.6@gmail.com>
2023-06-02 01:32:44 +0900

    first commit

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ use crossterm::{
 use image::io::Reader as ImageReader;
 use image::Pixel;
 use image::{open, GenericImageView, ImageBuffer, Rgba};
+use std::io::Write;
 use std::path::Path;
+use std::thread;
+use std::time::Duration;
 use std::{env, process::Command};
 
 fn main() {
@@ -17,58 +20,58 @@ fn main() {
 
     let ffmpegPath = "ffmpeg\\ffmpeg\\bin\\ffmpeg.exe";
 
-    // println!("{:?}", path);
+    let MovieToBMP = Command::new(ffmpegPath)
+        .arg("-i")
+        .arg(input_file)
+        .arg("-vcodec")
+        .arg("png")
+        .arg(output_pattern)
+        .output()
+        .expect("Failed is Codec");
 
-    // let MovieToBMP = Command::new(ffmpegPath)
-    //     .arg("-i")
-    //     .arg(input_file)
-    //     .arg("-vcodec")
-    //     .arg("png")
-    //     .arg(output_pattern)
-    //     .output()
-    //     .expect("Failed is Codec");
+    if MovieToBMP.status.success() {
+        println!("Success");
+    } else {
+        println!("Error");
+    }
 
-    // if MovieToBMP.status.success()
-    // {
-    //     println!("Success");
-    // }
-    // else {
-    //     println!("Error");
-    // }
+    let entry = output_dir.read_dir().expect("read_dir call failed");
+    let entry_count = entry.count();
 
-    for entry in output_dir.read_dir().expect("read_dir call failed") {
-        if let Ok(entry) = entry {
-        
+    for entry in 1..entry_count {
+        let path = format!("output_frames\\image_{}.png", entry);
+        let result = open(path);
 
-            let result = open(entry.path());
+        match result {
+            Ok(image) => {
+                let (width, height) = image.dimensions();
 
-            match result
-            {
-                Ok(image) =>
-                {
-                    let (width, height) = image.dimensions();
+                // 画像の各ピクセルに対して色情報を取得し、出力する
+                for y in 0..height {
+                    for x in 0..width {
+                        // ピクセルの色情報を取得する
+                        let pixel_color: Rgba<u8> = image.get_pixel(x, y).into();
 
-                    // 画像の各ピクセルに対して色情報を取得し、出力する
-                    for y in 0..height {
-                        for x in 0..width {
-                            // ピクセルの色情報を取得する
-                            let pixel_color: Rgba<u8> = image.get_pixel(x, y).into();
-
-                            // R, G, B の値を取得する
-                            let (red, green, blue, _) = pixel_color.channels4();
-                            print!("{}■", SetForegroundColor(Color::Rgb{r:red, g:green, b:blue}));
-                        }
-                        println!();
+                        // R, G, B の値を取得する
+                        let (red, green, blue, _) = pixel_color.channels4();
+                        print!(
+                            "{}■",
+                            SetForegroundColor(Color::Rgb {
+                                r: red,
+                                g: green,
+                                b: blue
+                            })
+                        );
                     }
-                    println!("{}", CLEAR_SCREEN);
+                    println!();
                 }
-
-                Err(err) =>
-                {
-                    println!("{}", err);
-                }
+                println!("{}", CLEAR_SCREEN);
+                thread::sleep(Duration::from_millis(30));
             }
-            
+
+            Err(err) => {
+                println!("{}", err);
+            }
         }
     }
 }
